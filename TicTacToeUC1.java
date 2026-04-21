77c8151845e2399c83fe6440ec0d19a6a67939ba
shreyadas9774-lab <shreyadas9774@gmail.com>
2026-04-21 10:58:21 +0530

    Added UC1 TicTacToe

---
+++ TicTacToeUC1.java
@@ -0,0 +1,24 @@
+public class TicTacToeUC1 {
+    public static void main(String[] args) {
+
+        // Create 3x3 board
+        char[][] board = new char[3][3];
+
+        // Initialize board with '-'
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+
+        // Print the board
+        System.out.println("Empty Tic-Tac-Toe Board:");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println(); // move to next row
+        }
+    }
+}
