7731a025925f543c15deb58f15d7b4a3b84b5e16
shreyadas9774-lab <shreyadas9774@gmail.com>
2026-04-21 11:23:57 +0530

    Added UC6 TicTacToe

---
+++ TicTacToeUC6.java
@@ -0,0 +1,36 @@
+public class TicTacToeUC6 {
+
+    // Method to place move
+    public static void placeMove(char[][] board, int row, int col, char symbol) {
+        board[row][col] = symbol;
+    }
+
+    // Method to print board (for checking)
+    public static void printBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+        };
+
+        int row = 1;
+        int col = 1;
+        char symbol = 'X';
+
+        // Place move
+        placeMove(board, row, col, symbol);
+
+        // Print updated board
+        printBoard(board);
+    }
+}
